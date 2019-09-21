5072006302b7b505a960a6afc6d04b426dcb918a
Max <mhandler1991@gmail.com>
2019-09-21 12:34:12 -0700

    General formatting fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,82 +1,86 @@
-// **************
-// Variables
-// **************
+$(document).ready(function () {
 
-// Project 1 Variables
-var project1 = $("#project1").attr("data-name");
-var project1date;
+  console.log("ready!");
 
-// Project 2 Variables
-var project2 = $("#project2").attr("data-name");
-var project2date;
+  // **************
+  // Variables
+  // **************
 
-// Project 3 Variables
-var project3 = $("#project3").attr("data-name");
-var project3date;
+  // Project 1 Variables
+  var project1 = $("#project1").attr("data-name");
+  var project1date;
 
-// Project 4 Variables
-var project4 = $("#project4").attr("data-name");
-var project4date;
+  // Project 2 Variables
+  var project2 = $("#project2").attr("data-name");
+  var project2date;
 
-// Project 5 Variables
-var project5 = $("#project5").attr("data-name");
-var project5date;
+  // Project 3 Variables
+  var project3 = $("#project3").attr("data-name");
+  var project3date;
 
-// Project 6 Variables
-var project6 = $("#project6").attr("data-name");
-var project6date;
+  // Project 4 Variables
+  var project4 = $("#project4").attr("data-name");
+  var project4date;
 
-var repo = function () {
-  $.ajax({
-    url: "https://api.github.com/users/mhandler1991/repos",
-    jsonp: true,
-    method: "GET",
-    dataType: "json",
-    success: function (res) {
-      console.log(res);
+  // Project 5 Variables
+  var project5 = $("#project5").attr("data-name");
+  var project5date;
 
-      // For Loop
-      for (i = 0; i < res.length; i++) {
+  // Project 6 Variables
+  var project6 = $("#project6").attr("data-name");
+  var project6date;
 
-        if (res[i].name == project1) {
+  var repo = function () {
+    $.ajax({
+      url: "https://api.github.com/users/mhandler1991/repos",
+      jsonp: true,
+      method: "GET",
+      dataType: "json",
+      success: function (res) {
+        console.log(res);
 
-          project1date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
-          $('#project1date').text(project1date);
+        // For Loop
+        for (i = 0; i < res.length; i++) {
 
-          // moment().format("MMM Do YY");  
+          if (res[i].name == project1) {
 
-        } else if (res[i].name == project2) {
+            project1date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
+            $('#project1date').text(project1date);
 
-          project2date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
-          $('#project2date').text(project2date);
+          } else if (res[i].name == project2) {
 
-        } else if (res[i].name == project3) {
+            project2date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
+            $('#project2date').text(project2date);
 
-          project3date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
-          $('#project3date').text(project3date);
+          } else if (res[i].name == project3) {
 
-        } else if (res[i].name == project4) {
+            project3date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
+            $('#project3date').text(project3date);
 
-          project4date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
-          $('#project4date').text(project4date);
+          } else if (res[i].name == project4) {
 
-        } else if (res[i].name == project5) {
+            project4date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
+            $('#project4date').text(project4date);
 
-          project5date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
-          $('#project5date').text(project5date);
+          } else if (res[i].name == project5) {
 
-        } else if (res[i].name == project6) {
+            project5date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
+            $('#project5date').text(project5date);
 
-          project6date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
-          $('#project6date').text(project6date);
+          } else if (res[i].name == project6) {
+
+            project6date = moment(res[i].updated_at).format('MMMM Do YYYY, h:mm:ss a');
+            $('#project6date').text(project6date);
+
+          }
+
 
         }
 
-
       }
+    });
+  }
 
-    }
-  });
-}
+  repo();
 
-repo();
+});
